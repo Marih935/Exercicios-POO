62fe6aea3ca68a780104d4eeb1eff5b03c0c3c9e
Mariana Cavalcanti Machado Anselmo <104873264+Marih935@users.noreply.github.com>
2023-03-08 17:21:56 -0300

    Create Ex-03.js

---
+++ 03 - Primitivas e Referências/Ex-03.js
@@ -0,0 +1,18 @@
+//take code snippets from the pool and place them into the blank lines in the code
+
+public class Triangle {
+	double area;
+	int height;
+	int lenght;
+	
+	public static void main(String[] args) {
+		int x = 0;
+		Triangle[] ta = new Triangle[4];
+		while(x < 4) {
+			ta[x] = new Triangle();
+			ta[x].height = (x + 1) * 2;
+			ta[x].lenght = x + 4;
+			ta[x]
+		}
+	}
+}
